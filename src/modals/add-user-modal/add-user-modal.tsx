9cca749ef35048840fa2e11378a35e6ffa7b403b
Artem Salamakha <a.salamaha@itransition.com>
2023-04-10 19:40:40 +0300

    add ability to update user info in the list

--- src/modals/add-user-modal/add-user-modal.tsx
+++ src/modals/add-user-modal/add-user-modal.tsx
@@ -1,5 +1,5 @@
 import { Button, Col, Form, Modal, Row } from "react-bootstrap";
-import { FormEventHandler } from "react";
+import { FormEventHandler, useState } from "react";
 import { addUser, updateUser } from "../../services/users";
 import { UserModel } from "../../types/user.model";
 import { useMutation } from "@tanstack/react-query";
@@ -9,11 +9,17 @@ interface AddUserModalProps {
 	show?: boolean;
 	onHide?: () => void;
 	onSent?: (data?: UserModel) => void;
-	userData?: UserInfoModel
+	userData?: UserInfoModel | null;
 }
 
 export const AddUserModal: React.FC<AddUserModalProps> = ({show, onHide, userData, onSent}) => {
 	const isEditMode: boolean = !!userData;
+	const [id] = useState(userData?.id);
+	const [firstName, setFirstName] = useState(userData?.first_name ?? '');
+	const [lastName, setLastName] = useState(userData?.last_name ?? '');
+	const [description, setDescription] = useState(userData?.description ?? '');
+	const [email, setEmail] = useState(userData?.email ?? '');
+	const [avatar, setAvatar] = useState(userData?.avatar ?? '');
 
 	const {mutate} = useMutation({
 		mutationFn: (userModel: UserModel) => {
@@ -30,16 +36,17 @@ export const AddUserModal: React.FC<AddUserModalProps> = ({show, onHide, userDat
 	const onSubmit: FormEventHandler<HTMLFormElement> = (event): void => {
 		event.preventDefault();
 
-		const formData = new FormData(event.target as HTMLFormElement);
-		const data = Object.fromEntries(formData);
-
 		const userModel: UserModel = {
-			description: data.description as string,
-			first_name: data.firstName as string,
-			last_name: data.lastName as string,
-			email: data.email as string,
-			avatar: data.avatar as string
+			description,
+			first_name: firstName,
+			last_name: lastName,
+			email,
+			avatar
 		};
+
+		if (id) {
+			userModel.id = id;
+		}
 
 		mutate(userModel);
 	};
@@ -61,13 +68,20 @@ export const AddUserModal: React.FC<AddUserModalProps> = ({show, onHide, userDat
 						<Col xs="6">
 							<Form.Group>
 								<Form.Label>First Name</Form.Label>
-								<Form.Control type="text" name="firstName" autoFocus/>
+								<Form.Control type="text"
+								              name="firstName"
+								              autoFocus
+								              value={firstName}
+								              onChange={(e) => setFirstName(e.target.value)}/>
 							</Form.Group>
 						</Col>
 						<Col xs="6">
 							<Form.Group>
 								<Form.Label>Last Name</Form.Label>
-								<Form.Control type="text" name="lastName"/>
+								<Form.Control type="text"
+								              name="lastName"
+								              value={lastName}
+								              onChange={(e) => setLastName(e.target.value)}/>
 							</Form.Group>
 						</Col>
 					</Row>
@@ -75,13 +89,19 @@ export const AddUserModal: React.FC<AddUserModalProps> = ({show, onHide, userDat
 						<Col xs="6">
 							<Form.Group>
 								<Form.Label>Email</Form.Label>
-								<Form.Control type="text" name="email"/>
+								<Form.Control type="text"
+								              name="email"
+								              value={email}
+								              onChange={(e) => setEmail(e.target.value)}/>
 							</Form.Group>
 						</Col>
 						<Col xs="6">
 							<Form.Group>
 								<Form.Label>Avatar Link</Form.Label>
-								<Form.Control type="text" name="avatar"/>
+								<Form.Control type="text"
+								              name="avatar"
+								              value={avatar}
+								              onChange={(e) => setAvatar(e.target.value)}/>
 							</Form.Group>
 						</Col>
 					</Row>
@@ -90,7 +110,11 @@ export const AddUserModal: React.FC<AddUserModalProps> = ({show, onHide, userDat
 						controlId="exampleForm.ControlTextarea1"
 					>
 						<Form.Label>Description</Form.Label>
-						<Form.Control as="textarea" rows={3} name="description"/>
+						<Form.Control as="textarea"
+						              rows={3}
+						              name="description"
+						              value={description}
+						              onChange={(e) => setDescription(e.target.value)}/>
 					</Form.Group>
 				</Form>
 			</Modal.Body>
--- src/routes/users-list/users-list.tsx
+++ src/routes/users-list/users-list.tsx
@@ -1,14 +1,14 @@
 import "./users-list.css";
 import { Button, Col, FormControl, Pagination, Row } from "react-bootstrap";
-import { AddUserModal } from "../../modals/add-user-modal/add-user-modal";
 import * as React from "react";
-import { Suspense, useState } from "react";
+import { useState } from "react";
 import { getList } from "../../services/users";
 import { useQuery } from "@tanstack/react-query";
 import { UsersListItem } from "../../types/users-list-item";
 import { Link } from "react-router-dom";
 import { UserAvatar } from "../../components/avatar/user-avatar";
 import { UsersListResponse } from "../../types/users-list-response";
+import { AddUserModal } from "../../modals/add-user-modal/add-user-modal";
 
 export const UsersList = () => {
 	const perPage = 3;
@@ -17,8 +17,9 @@ export const UsersList = () => {
 	const [searchText, setSearchText] = useState<string>('');
 	const [users, setUsers] = useState<UsersListItem[]>([]);
 	const [pages, setPages] = useState(1);
+	const [activeUser, setActiveUser] = useState<null | UsersListItem>();
 
-	const {isSuccess, isLoading} = useQuery({
+	const {isSuccess, isLoading, refetch} = useQuery({
 		queryKey: ['users', page, perPage, searchText],
 		queryFn: () => getList(page, perPage, searchText),
 		onSuccess: (data: UsersListResponse) => {
@@ -41,7 +42,7 @@ export const UsersList = () => {
 		setPage(page);
 	};
 
-	const renderPaginationItens = (): JSX.Element[] => {
+	const renderPaginationItems = (): JSX.Element[] => {
 		const list: JSX.Element[] = [];
 
 		for (let i = 1; i < pages + 1; i++) {
@@ -52,6 +53,17 @@ export const UsersList = () => {
 		return list;
 	};
 
+	const editUser = (user: UsersListItem): void => {
+		setActiveUser(user);
+		setShowAddUserModal(true);
+	};
+
+	const onUserUpdated = () => {
+		setActiveUser(null);
+		setShowAddUserModal(false);
+		refetch();
+	};
+
 	const renderUsers = (): JSX.Element | JSX.Element[] => {
 		if (isLoading && !users) {
 			return <div>Loading ...</div>;
@@ -60,12 +72,13 @@ export const UsersList = () => {
 		if (isSuccess && users) {
 			return users.map((user: UsersListItem) =>
 				<div className="list-item" key={user.id}>
-					<UserAvatar avatarPath={user.avatar} link={`/user/${user.id}`}/>
+					<UserAvatar avatarPath={user.avatar} link={`/users/${user.id}`}/>
 					<div className="info">
 						<div className="name">
-							<Link to={`/user/${user.id}`}>
+							<Link to={`/users/${user.id}`}>
 								{user.first_name} {user.last_name}
 							</Link>
+							<Button className="edit-btn" variant="link" onClick={() => editUser(user)}>Edit</Button>
 						</div>
 						<div className="email">{user.email}</div>
 						<div className="short-info">
@@ -100,13 +113,14 @@ export const UsersList = () => {
 
 			{renderUsers()}
 
-			<Pagination>{renderPaginationItens()}</Pagination>
+			<Pagination>{renderPaginationItems()}</Pagination>
 
-			<Suspense>
-				<AddUserModal show={showAddUserModal}
-				              onHide={() => setShowAddUserModal(false)}
-				              onSent={onUserAdded}/>
-			</Suspense>
+			{showAddUserModal &&
+      <AddUserModal show={showAddUserModal}
+                    onHide={() => setShowAddUserModal(false)}
+                    onSent={activeUser ? onUserUpdated : onUserAdded}
+                    userData={activeUser}/>
+			}
 		</section>
 	);
 }
