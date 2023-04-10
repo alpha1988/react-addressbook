850e9d44e098a8e37eb86561ae8d9054fdd3b2c9
Artem Salamakha <a.salamaha@itransition.com>
2023-04-10 17:39:15 +0300

    refactor UserAvatar component template

--- src/components/avatar/user-avatar.tsx
+++ src/components/avatar/user-avatar.tsx
@@ -9,11 +9,10 @@ interface UserAvatarProps {
 
 export const UserAvatar: React.FC<UserAvatarProps> = ({avatarPath, link}) => {
 	const imgLayout = <img src={avatarPath} alt=""/>;
-	const layout = link ? <Link to={link}>{imgLayout}</Link> : imgLayout;
 
 	return (
 		<div className="user-avatar">
-			{layout}
+			{link ? <Link to={link}>{imgLayout}</Link> : imgLayout}
 		</div>
 	);
 }
